d713b3769bfd28c7d1ae17f61486abf8c74362d4
Somanath Patil <patilsomnath86@gmail.com>
2017-01-15 16:31:14 -0600

    updated comments

--- ex3.2.py
+++ ex3.2.py
@@ -1,6 +1,6 @@
 #https://books.trinket.io/pfe/03-conditional.html
 '''Exercise 2: Rewrite your pay program using try and except so that your program handles non-numeric input gracefully by printing 
-a message and exiting the program. The following shows two executions of the program:'''
+a message and exiting the program. '''
 try:
 	hrs = float(input("Enter Hours:"))
 	rte = float(input("Enter rate per Hour:"))
