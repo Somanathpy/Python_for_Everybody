e540a5535289fce9076ea6e69fae51d6a5015f2d
Somanath Patil <patilsomnath86@gmail.com>
2017-01-17 23:24:52 -0600

    exercise 6 from lesson 4

--- ex4.2.py
+++ ex4.2.py
@@ -1,6 +1,8 @@
 #https://books.trinket.io/pfe/04-functions.html
 '''Exercise 6: Rewrite your pay computation with time-and-a-half for overtime and create a function called computepay 
 which takes two parameters (hours and rate).'''
+hours = float(input("enter hours:"))
+rate = float(input("enter rate:"))
 
 def computepay(hours,rate):
 	#hours = float(input("enter hours:"))
@@ -12,4 +14,4 @@ def computepay(hours,rate):
 	return pay
 	
 
-print(computepay(45,10))
+print(computepay(hours,rate))
