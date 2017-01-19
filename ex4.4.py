c2422ccf79e03aeb9a660b91d904bd93ac56477a
Somanath Patil <patilsomnath86@gmail.com>
2017-01-18 23:51:42 -0600

    excersie 5 from 5

--- ex4.4.py
+++ ex4.4.py
@@ -1,3 +1,7 @@
+#https://books.trinket.io/pfe/05-iterations.html
+'''Exercise 2: Write another program that prompts for a list of numbers as above and at the 
+end prints out both the maximum and minimum of the numbers instead of the average.'''
+
 max = None
 min = None
 while True:
